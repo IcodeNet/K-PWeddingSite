0461bf8c119cb58d10a1f0d4cd51ac5c94b73c7d
Paul <p@ulharding.me>
2017-01-02 17:48:17 +0000

    AddingContent

---
+++ src/Wedding.Site/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace Wedding.Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Rsvp()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+
+        public IActionResult TheDay()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Gifts()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Accommodation()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
